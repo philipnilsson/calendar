59e925e9ce2a0f2ecd0fca727b5315e3a5bee773
Philip Nilsson <philip@helloproper.com>
2020-11-20 10:18:26 +0200

    :sparkles: Remove some margins from headers.

--- src/stories/atoms/typography/H1.tsx
+++ src/stories/atoms/typography/H1.tsx
@@ -3,6 +3,8 @@ import styled from 'styled-components'
 const dark = '#222'
 
 export const H1 = styled.h1`
+  margin: 0;
+  line-height: 1;  
   font-family: Roboto, sans-serif;
   font-weight: 300;
   font-size: 145%;
--- src/stories/atoms/typography/H3.tsx
+++ src/stories/atoms/typography/H3.tsx
@@ -3,6 +3,8 @@ import styled from 'styled-components'
 const medium = '#8a8a85'
 
 export const H3 = styled.h3`  
+  margin: 0;
+  line-height: 1;
   font-family: Roboto, sans-serif;
   text-transform: uppercase;
   font-size: 75%;
