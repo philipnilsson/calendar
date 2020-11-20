db5ca4aaee2ebf8a66b9c42823a8d25f2eb80d90
Philip Nilsson <philip@helloproper.com>
2020-11-20 10:18:03 +0200

    :recycle: Change grid-gap to borders.

--- src/stories/atoms/layout/Grid.tsx
+++ src/stories/atoms/layout/Grid.tsx
@@ -4,30 +4,30 @@ import styled from 'styled-components'
 export type GridProps = {
   rows: number,
   cols: number,
-  renderCell: (i: number, j: number) => ReactNode
+  renderCell: (i: number, j: number) => ReactNode,
 }
 
 const StyledGrid = styled.div<{ cols: number } >`
   display: grid;
+  background: white;
   grid-template-columns: repeat(${props => props.cols}, 1fr);
 `
 
-const Cell = styled.div`
+const GridCell = styled.div`
   width: 100%;
   height: 100%;
   background: white;
-  border-left: 1px solid;
-  border-bottom: 1px solid;
-  border-color: #e2e1df;
+  border-left: 1px solid #e2e1df;
+  border-bottom: 1px solid #e2e1df;
 `
 
-export function Grid(props: GridProps) {
+export const Grid = styled(function Grid({ renderCell, cols, rows, ...props }: GridProps) {
   return (
-    <StyledGrid cols={props.cols}>
-      {[...Array(props.rows)].map((_, i) =>
-        [...Array(props.cols)].map((_, j) =>
-          <Cell>{props.renderCell(i, j)}</Cell>))
+    <StyledGrid cols={cols} {...props}>
+      {[...Array(rows)].map((_, i) =>
+        [...Array(cols)].map((_, j) =>
+          <GridCell>{renderCell(i, j)}</GridCell>))
       }
     </StyledGrid>
   )
-}
+})``
