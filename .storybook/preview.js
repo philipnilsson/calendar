012e1e6c7c114b1eae51f362f423a7fda39df5b3
Philip Nilsson <philip@helloproper.com>
2020-11-20 21:33:13 +0200

    :bug: Add the theme provider to the styleguie.

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,14 @@
+import React from "react";
+import { ThemeProvider } from 'styled-components';
+import { light } from '../src/stories/theme/theme.ts'
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={light}>
+      <Story />
+    </ThemeProvider>
+  ),
+];
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
